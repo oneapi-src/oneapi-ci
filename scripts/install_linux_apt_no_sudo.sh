554d5c2ec4bb328522bb878a4505792c5905cf62
Zakharov <maxim.m.zakharov@intel.com>
2020-09-04 17:29:22 +0300

    update

--- scripts/install_linux_apt_no_sudo.sh
+++ scripts/install_linux_apt_no_sudo.sh
@@ -7,6 +7,6 @@ apt-get update
 apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+echo "deb https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
 apt-get update
 apt-get install -y $COMPONENTS
