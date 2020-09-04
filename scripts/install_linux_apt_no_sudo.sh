7dc2fd57c66e3d3bbf8c94e79e56d05cf1d543d0
Zakharov <maxim.m.zakharov@intel.com>
2020-09-04 17:19:55 +0300

    update

--- scripts/install_linux_apt_no_sudo.sh
+++ scripts/install_linux_apt_no_sudo.sh
@@ -3,10 +3,10 @@
 COMPONENTS=$(echo $1 | sed "s/,/ /g")
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg
+apt-get update
+apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
-sudo apt-get update
-sudo apt-get install -y $COMPONENTS
+apt-get update
+apt-get install -y $COMPONENTS
