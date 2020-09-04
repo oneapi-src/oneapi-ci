ca6284b24519611372365aca20aebe26cd167cc5
Zakharov <maxim.m.zakharov@intel.com>
2020-09-04 18:05:49 +0300

    update

--- scripts/install_debug_windows.bat
+++ scripts/install_debug_windows.bat
@@ -6,6 +6,6 @@ start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 type extract.log
 webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept --continue-with-optional-error=yes -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
 set EXITCODE=%ERRORLEVEL%
-for /f usebackq %F in (`dir /b bootstrapper*`) do type %F
-for /f usebackq %F in (`dir /b installer*`) do type %F
+for /f usebackq %%F in (`dir /b bootstrapper*`) do type %%F
+for /f usebackq %%F in (`dir /b installer*`) do type %%F
 exit /b %EXITCODE%
