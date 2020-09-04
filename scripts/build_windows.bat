1e28fcc05b2d893ac875d486c461fb297693b7d8
Zakharov <maxim.m.zakharov@intel.com>
2020-09-04 20:00:58 +0300

    update

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -5,11 +5,12 @@ cd src
 cd %LANGUAGE%
 
 IF "%VS_VER%"=="2017_build_tools" (
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 )
 
 IF "%VS_VER%"=="2019_build_tools" (
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 )
 
-"C:\Program Files (x86)\Intel\oneAPI\setvars.bat" && nmake /f Makefile.nmake"
+@call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
+nmake /f Makefile.nmake"
