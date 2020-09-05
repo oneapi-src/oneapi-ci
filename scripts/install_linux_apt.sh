eae69f583d3ba6489ee47f91e98263530cc9ce87
Zakharov <maxim.m.zakharov@intel.com>
2020-09-05 13:22:57 +0300

    update

--- scripts/install_linux_apt.sh
+++ scripts/install_linux_apt.sh
@@ -4,7 +4,7 @@ COMPONENTS=$(echo $1 | sed "s/,/ /g")
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg
+sudo apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg git
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
--- scripts/install_linux_apt_no_sudo.sh
+++ scripts/install_linux_apt_no_sudo.sh
@@ -4,7 +4,7 @@ COMPONENTS=$(echo $1 | sed "s/,/ /g")
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg
+apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg git
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 echo "deb https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list
