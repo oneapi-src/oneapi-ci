4e6452993b448df994bc122fa93b1942f316be5b
Zakharov <maxim.m.zakharov@intel.com>
2020-09-05 12:44:19 +0300

    update

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -2,6 +2,21 @@
 
 LANGUAGE=$1
 
-cd src/$LANGUAGE
+git clone https://github.com/oneapi-src/oneAPI-samples.git
+
 source /opt/intel/oneapi/setvars.sh
-make
+
+case $LANGUAGE in
+c++)
+  cd oneAPI-samples/DirectProgramming/C++/CompilerInfrastructure/Intrinsics
+  make && make run
+  ;;
+fortran)
+  cd oneAPI-samples/DirectProgramming/Fortran/CombinationalLogic/openmp-primes
+  make && make run
+  ;;
+dpc++)
+  cd oneAPI-samples/DirectProgramming/DPC++/DenseLinearAlgebra/vector-add
+  make all && make run
+  ;;
+esac
