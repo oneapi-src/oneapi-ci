ce6130f119b6e3d95b3048c47b79173112d39020
Zakharov <maxim.m.zakharov@intel.com>
2020-09-05 15:39:08 +0300

    update

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -20,12 +20,9 @@ goto exit
 
 :cpp
 cd oneAPI-samples\DirectProgramming\C++\CompilerInfrastructure\Intrinsics
-icl -O2 -c -o intrin_dot_sample.o  src\intrin_dot_sample.cpp
-icl -O2 intrin_dot_sample.o -o intrin_dot_sample.exe
-icl -O2 -c -o intrin_double_sample.o  src\intrin_double_sample.cpp
-icl -O2 intrin_double_sample.o -o intrin_double_sample.exe
-icl -O2 -c -o intrin_ftz_sample.o  src\intrin_ftz_sample.cpp
-icl -O2 intrin_ftz_sample.o -o intrin_ftz_sample.exe
+icl -O2 src\intrin_dot_sample.cpp
+icl -O2 src\intrin_double_sample.cpp
+icl -O2 src\intrin_ftz_sample.cpp
 intrin_dot_sample.exe
 intrin_double_sample.exe
 intrin_ftz_sample.exe
@@ -33,8 +30,7 @@ goto exit
 
 :fortran
 cd oneAPI-samples\DirectProgramming\Fortran\CombinationalLogic\openmp-primes
-ifort -O2 -c -fpp -qopenmp -o openmp_sample.o  src/openmp_sample.f90
-ifort -O2 -fpp -qopenmp openmp_sample.o -o openmp_sample.exe
+ifort -O2 -fpp -qopenmp src\openmp_sample.f90
 openmp_sample.exe
 goto exit
 
