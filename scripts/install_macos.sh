2340745c130d9579324a4ded0f0ea17e6d10492f
Zakharov <maxim.m.zakharov@intel.com>
2020-10-26 11:28:10 +0300

    saving install logs in case on failure on macos

--- scripts/install_macos.sh
+++ scripts/install_macos.sh
@@ -9,4 +9,4 @@ COMPONENTS=$2
 
 curl --output webimage.dmg --url $URL
 hdiutil attach webimage.dmg
-sudo /Volumes/$(basename $URL .dmg)/bootstrapper.app/Contents/MacOS/bootstrapper -s --action install --components=$COMPONENTS --eula=accept --continue-with-optional-error=yes
+sudo /Volumes/$(basename $URL .dmg)/bootstrapper.app/Contents/MacOS/bootstrapper -s --action install --components=$COMPONENTS --eula=accept --continue-with-optional-error=yes --log-dir=.
