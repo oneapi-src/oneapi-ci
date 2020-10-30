2ab551fa9852343dc772fb1c644187956ef738e3
Zakharov <maxim.m.zakharov@intel.com>
2020-10-30 09:58:46 +0300

    checking if the windows instability is related to artifacts

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -5,6 +5,6 @@ REM SPDX-License-Identifier: MIT
 set URL=%1
 set COMPONENTS=%2
 
-curl.exe --output webimage.exe --url %URL%  --retry 5 --retry-delay 5
+curl.exe --output webimage.exe --url %URL% --retry 5 --retry-delay 5
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept --continue-with-optional-error=yes -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
