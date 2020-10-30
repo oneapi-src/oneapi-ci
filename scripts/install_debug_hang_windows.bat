f5db64c4f51176f6cd89db42e415bbed911f74f3
Zakharov <maxim.m.zakharov@intel.com>
2020-10-30 11:17:07 +0300

    trying debug hang

--- scripts/install_debug_hang_windows.bat
+++ scripts/install_debug_hang_windows.bat
@@ -5,9 +5,9 @@ REM SPDX-License-Identifier: MIT
 set URL=%1
 set COMPONENTS=%2
 set TIMEOUT=%3
-if "%TIMEOUT%"=="" set TIMEOUT=300
+if "%TIMEOUT%"=="" set TIMEOUT=1000
 
-curl.exe --output webimage.exe --url %URL%
+curl.exe --output webimage.exe --url %URL% --retry 5 --retry-delay 5
 start /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 type extract.log
 start /b webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept --continue-with-optional-error=yes -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
