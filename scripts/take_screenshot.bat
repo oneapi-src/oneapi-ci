0b17b05f70c28307e387fd14f89472dc1ab6f136
Zakharov <maxim.m.zakharov@intel.com>
2020-11-05 15:37:22 +0300

    getting screenshot of another window

--- scripts/take_screenshot.bat
+++ scripts/take_screenshot.bat
@@ -4,3 +4,4 @@ REM SPDX-License-Identifier: MIT
 
 curl.exe --output screenshot-cmd.exe --url https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/screenshot-cmd/screenshot-cmd.exe --retry 5 --retry-delay 5
 screenshot-cmd.exe
+move screenshot.png %1
