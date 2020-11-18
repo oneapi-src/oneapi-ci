1a6ea4f42ea7a6f17bf81cd7a1d69fd2ae71661a
Zakharov <maxim.m.zakharov@intel.com>
2020-11-18 23:27:30 +0300

    trying different way

--- scripts/install_prerequisites_linux_apt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# SPDX-FileCopyrightText: 2020 Intel Corporation
-#
-# SPDX-License-Identifier: MIT
-
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-sudo apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg git
