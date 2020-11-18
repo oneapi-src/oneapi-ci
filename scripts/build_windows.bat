2cdcd84963b89e4b775904ee97f4067b06bdce7d
Zakharov <maxim.m.zakharov@intel.com>
2020-11-18 12:49:15 +0300

    fix -

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -15,7 +15,7 @@ IF "%VS_VER%"=="2019_build_tools" (
 
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\env\vars.bat"
 
-git clone -–depth 1 https://github.com/oneapi-src/oneAPI-samples.git
+git clone --depth 1 https://github.com/oneapi-src/oneAPI-samples.git
 
 if "%LANGUAGE%" == "c++" goto cpp
 if "%LANGUAGE%" == "fortran" goto fortran
