6207de0c1d5de6d7ad5e657b8f99dc0a61aafec1
Zakharov <maxim.m.zakharov@intel.com>
2020-11-18 23:38:44 +0300

    trying yet another approach

---
+++ scripts/remove_unused_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+rm -rf /opt/intel/oneapi/compiler/2021.1-beta10/linux/compiler/lib/ia32_lin
+rm -rf /opt/intel/oneapi/compiler/2021.1-beta10/linux/bin/ia32
+rm -rf /opt/intel/oneapi/compiler/2021.1-beta10/linux/lib/emu
+rm -rf /opt/intel/oneapi/compiler/2021.1-beta10/linux/lib/oclfpga
