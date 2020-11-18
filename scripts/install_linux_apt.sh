6747432b6e61e75503b4d71b7baebaddfe823430
Zakharov <maxim.m.zakharov@intel.com>
2020-11-18 22:20:35 +0300

    fix 6

--- scripts/install_linux_apt.sh
+++ scripts/install_linux_apt.sh
@@ -7,6 +7,5 @@
 COMPONENTS=$(echo "$1" | sed "s/,/ /g")
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
 sudo apt-get install -y build-essential pkg-config cmake ca-certificates gnupg git
 sudo apt-get install -y "$COMPONENTS"
--- scripts/install_linux_apt_no_sudo.sh
+++ scripts/install_linux_apt_no_sudo.sh
@@ -7,6 +7,5 @@
 COMPONENTS=$(echo "$1" | sed "s/,/ /g")
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
 apt-get install -y build-essential pkg-config cmake ca-certificates gnupg git
 apt-get install -y "$COMPONENTS"
