7c10c9a4f68b235eecd6fe62efa8885e4947243f
Zakharov <maxim.m.zakharov@intel.com>
2020-11-19 12:17:39 +0300

    test

--- scripts/cache_exclude_windows.bat
+++
@@ -1,8 +0,0 @@
-REM SPDX-FileCopyrightText: 2020 Intel Corporation
-REM
-REM SPDX-License-Identifier: MIT
-
-rd /s /q "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\compiler\lib\ia32_win"
-rd /s /q "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\bin\intel64_ia32"
-rd /s /q "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\emu"
-rd /s /q "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\oclfpga"
---
+++ scripts/cache_exclude_windows.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\compiler\lib\ia32_win"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\bin\intel64_ia32"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\emu"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\oclfpga"
