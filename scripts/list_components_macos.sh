a8db81f1ecf0cd64eccc2043292fa390cedcdd6e
Zakharov <maxim.m.zakharov@intel.com>
2020-11-24 16:42:21 +0300

    checking list-components 3

--- scripts/list_components_macos.sh
+++ scripts/list_components_macos.sh
@@ -9,3 +9,4 @@ URL=$1
 curl --output webimage.dmg --url "$URL" --retry 5 --retry-delay 5
 hdiutil attach webimage.dmg
 sudo /Volumes/"$(basename "$URL" .dmg)"/bootstrapper.app/Contents/MacOS/bootstrapper --list-components
+hdiutil detach webimage.dmg
