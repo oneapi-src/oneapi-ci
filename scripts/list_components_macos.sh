0f528a18d110dbb3fe0450f8fe2161d5d8f8a9bf
Zakharov <maxim.m.zakharov@intel.com>
2020-11-24 16:24:40 +0300

    checking list-components

---
+++ scripts/list_components_macos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+URL=$1
+
+curl --output webimage.dmg --url "$URL" --retry 5 --retry-delay 5
+hdiutil attach webimage.dmg
+sudo /Volumes/"$(basename "$URL" .dmg)"/bootstrapper.app/Contents/MacOS/bootstrapper --list-components
