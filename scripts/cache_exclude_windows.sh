cd8eed7ba70083dbf8c68950cb8353ea94b86c26
Zakharov <maxim.m.zakharov@intel.com>
2020-12-03 22:32:52 +0300

    fixed caching

--- scripts/cache_exclude_windows.sh
+++ scripts/cache_exclude_windows.sh
@@ -4,9 +4,11 @@
 #
 # SPDX-License-Identifier: MIT
 
-rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\compiler\lib\ia32_win"
-rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\bin\intel64_ia32"
-rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\emu"
-rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\oclfpga"
-rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\ocloc"
-rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\2021.1-beta10\windows\lib\x86"
+ONEAPI_RELEASE=$1
+
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\$ONEAPI_RELEASE\windows\compiler\lib\ia32_win"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\$ONEAPI_RELEASE\windows\bin\intel64_ia32"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\$ONEAPI_RELEASE\windows\lib\emu"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\$ONEAPI_RELEASE\windows\lib\oclfpga"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\$ONEAPI_RELEASE\windows\lib\ocloc"
+rm -rf "C:\Program Files (x86)\Intel\oneAPI\compiler\$ONEAPI_RELEASE\windows\lib\x86"
