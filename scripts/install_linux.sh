b57545b217e7e10e3eb295866319b2c083007d07
Zakharov <maxim.m.zakharov@intel.com>
2020-12-04 21:50:27 +0300

    switch to sudo install

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -12,8 +12,8 @@ chmod +x webimage.sh
 ./webimage.sh -x -f webimage_extracted --log extract.log
 WEBIMAGE_NAME=$(ls -1 webimage_extracted/)
 if [ -z "$COMPONENTS" ]; then
-  webimage_extracted/"$WEBIMAGE_NAME"/bootstrapper -s --action install --eula=accept --continue-with-optional-error=yes --log-dir=.
+  sudo webimage_extracted/"$WEBIMAGE_NAME"/bootstrapper -s --action install --eula=accept --continue-with-optional-error=yes --log-dir=.
 else
-  webimage_extracted/"$WEBIMAGE_NAME"/bootstrapper -s --action install --components="$COMPONENTS" --eula=accept --continue-with-optional-error=yes --log-dir=.
+  sudo webimage_extracted/"$WEBIMAGE_NAME"/bootstrapper -s --action install --components="$COMPONENTS" --eula=accept --continue-with-optional-error=yes --log-dir=.
 fi
 rm -rf webimage.sh webimage_extracted
