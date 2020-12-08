17e58503fbb65b632b009217977ab63bd2848f8b
Zakharov <maxim.m.zakharov@intel.com>
2020-12-08 18:33:45 +0300

    fix

--- scripts/install_linux_yum_ubuntu.sh
+++ scripts/install_linux_yum_ubuntu.sh
@@ -4,7 +4,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-sudo apt-get install -y yum
 tee > /tmp/oneAPI.repo << EOF
 [oneAPI]
 name=Intel(R) oneAPI repository
@@ -14,7 +13,6 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 EOF
-sudo mkdir /etc/yum.repos.d/
 sudo mv /tmp/oneAPI.repo /etc/yum.repos.d/
-sudo yum -y --disablerepo="*" --enablerepo="oneAPI" list available
-sudo yum -y install intel-basekit
+sudo dnf -y --disablerepo="*" --enablerepo="oneAPI" list available
+sudo dnf -y install intel-basekit
