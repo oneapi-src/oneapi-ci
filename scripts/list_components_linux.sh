20e1d712bb20be327eb3543ccac39b01e1af9454
Zakharov <maxim.m.zakharov@intel.com>
2020-12-09 17:07:28 +0300

    Added linux installer examples, other improvements

--- scripts/list_components_linux.sh
+++ scripts/list_components_linux.sh
@@ -9,6 +9,9 @@ URL=$1
 curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
 ./webimage.sh -x -f webimage_extracted --log extract.log
+rm -rf webimage.sh
 WEBIMAGE_NAME=$(ls -1 webimage_extracted/)
 webimage_extracted/"$WEBIMAGE_NAME"/bootstrapper --list-components
-rm -rf webimage.sh
+installer_exit_code=$?
+rm -rf webimage_extracted
+exit $installer_exit_code
