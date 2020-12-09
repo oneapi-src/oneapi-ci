9e785f189500f1aceb86dc7ea7ab9be4e43fda03
Zakharov <maxim.m.zakharov@intel.com>
2020-12-09 19:11:13 +0300

    test other compilers

---
+++ scripts/build_linux2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+LANGUAGE=$1
+ONEAPI_RELEASE=$2
+
+git clone --depth 1 https://github.com/oneapi-src/oneAPI-samples.git
+
+# shellcheck source=/dev/null
+source /opt/intel/oneapi/compiler/"$ONEAPI_RELEASE"/env/vars.sh
+
+case $LANGUAGE in
+c++)
+  icx src/c++/hello.cpp -lstdc++
+  ./a.out
+  ;;
+fortran)
+  ifx src/fortran/hello.f90
+  ./a.out
+  ;;
+dpc++)
+# shellcheck source=/dev/null
+  source /opt/intel/oneapi/tbb/"$ONEAPI_RELEASE"/env/vars.sh
+  cd oneAPI-samples/DirectProgramming/DPC++/DenseLinearAlgebra/vector-add
+  make all && make run
+  ;;
+esac
