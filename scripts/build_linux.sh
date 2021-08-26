feff25e2b6c8393bfa2f29660a50516c3a7bcbeb
Zakharov <maxim.m.zakharov@intel.com>
2021-08-26 12:16:45 +0300

    specifying tag for samples as 2021.3.0

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,8 +5,9 @@
 # SPDX-License-Identifier: MIT
 
 LANGUAGE=$1
+SAMPLES_TAG=$2
 
-git clone --depth 1 https://github.com/oneapi-src/oneAPI-samples.git
+git clone --depth 1 --branch "$SAMPLES_TAG" https://github.com/oneapi-src/oneAPI-samples.git
 
 #shellcheck disable=SC2010
 LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
