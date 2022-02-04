212861aec4069850b257e39664cdc89a0b6fbd1b
Zakharov <maxim.m.zakharov@intel.com>
2022-02-04 18:59:35 +0300

    2022.1.2

--- scripts/restore_registry.bat
+++ scripts/restore_registry.bat
@@ -2,6 +2,6 @@ REM SPDX-FileCopyrightText: 2021 Intel Corporation
 REM
 REM SPDX-License-Identifier: MIT
 
-COMPILER_VERSION=$1
+set COMPILER_VERSION=%1
 
-reg add HKLM\Software\Khronos\OpenCL\Vendors /v "C:\Program Files (x86)\Intel\oneAPI\compiler\$COMPILER_VERSION\windows\lib\x64\intelocl64.dll" /t REG_DWORD /d 0 /f
+reg add HKLM\Software\Khronos\OpenCL\Vendors /v "C:\Program Files (x86)\Intel\oneAPI\compiler\%COMPILER_VERSION%\windows\lib\x64\intelocl64.dll" /t REG_DWORD /d 0 /f
