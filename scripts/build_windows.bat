d5c37731d006a99229eaa390eb5d08b6bb79cacb
Zakharov <maxim.m.zakharov@intel.com>
2022-03-06 17:14:52 +0300

    diff win

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -13,6 +13,11 @@ IF "%VS_VER%"=="2017_build_tools" (
 IF "%VS_VER%"=="2019_build_tools" (
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 )
+
+IF "%VS_VER%"=="2022_build_tools" (
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+)
+
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
