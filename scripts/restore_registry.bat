cb96434bf9a51275aa858d3af42ce833d0a7b2a2
Zakharov <maxim.m.zakharov@intel.com>
2022-03-12 14:35:01 +0300

    Multiple bugsfixes and updates, incl. fix for dpcpp Windows caching

---
+++ scripts/restore_registry.bat
@@ -0,0 +1,7 @@
+REM SPDX-FileCopyrightText: 2022 Intel Corporation
+REM
+REM SPDX-License-Identifier: MIT
+
+set COMPILER_VERSION=%1
+
+reg add HKLM\Software\Khronos\OpenCL\Vendors /v "C:\Program Files (x86)\Intel\oneAPI\compiler\%COMPILER_VERSION%\windows\lib\x64\intelocl64.dll" /t REG_DWORD /d 0 /f
