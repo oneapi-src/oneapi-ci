7fdddd0c137cc2c4205ac206a35432d95770875d
Robert Cohn <robert.s.cohn@intel.com>
2022-05-02 12:34:52 -0400

    switch to 2022.2 staging

--- scripts/setup_apt_repo_linux.sh
+++ scripts/setup_apt_repo_linux.sh
@@ -6,5 +6,5 @@
 
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+echo "deb https://installer.sandbox.intel.com/staging/apt all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/oneAPI.list" -o APT::Get::List-Cleanup="0"
