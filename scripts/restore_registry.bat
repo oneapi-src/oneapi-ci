b8982d1c7a1fcb3f3d3631d70785e7552c65b5f6
Zakharov, Maxim M <maxim.m.zakharov@intel.com>
2022-11-10 21:15:48 +0100

    check 3

--- scripts/restore_registry.bat
+++ scripts/restore_registry.bat
@@ -5,5 +5,3 @@ REM SPDX-License-Identifier: MIT
 set COMPILER_VERSION=%1
 
 reg add HKLM\Software\Khronos\OpenCL\Vendors /v "C:\Program Files (x86)\Intel\oneAPI\compiler\%COMPILER_VERSION%\windows\lib\x64\intelocl64.dll" /t REG_DWORD /d 0 /f
-type "C:\Program Files (x86)\Intel\oneAPI\compiler\%COMPILER_VERSION%\windows\lib\x64\cl.cfg"
-type "C:\Program Files (x86)\Intel\oneAPI\compiler\%COMPILER_VERSION%\windows\lib\x64\cl.fpga_emu.cfg"
