4b53c8378785a307eea5dcf516430e433bbdcaf1
Zakharov, Maxim M <maxim.m.zakharov@intel.com>
2022-12-20 14:00:51 +0100

    fix10

--- scripts/setup_apt_repo_linux.sh
+++ scripts/setup_apt_repo_linux.sh
@@ -6,7 +6,7 @@
 
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
-echo "deb https://apt.repos.intel.com/staging/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/oneAPI.list" -o APT::Get::List-Cleanup="0"
 echo "deb https://apt.repos.intel.com/mpi all main" | sudo tee /etc/apt/sources.list.d/mpi.list
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mpi.list" -o APT::Get::List-Cleanup="0"
