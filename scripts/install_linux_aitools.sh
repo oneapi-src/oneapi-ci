bc5b898960184ca32d650959f6f918c20adb7c9f
Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>
2024-03-27 18:15:24 -0700

    Change install command for aitools
    Signed-off-by: Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>

--- scripts/install_linux_aitools.sh
+++ scripts/install_linux_aitools.sh
@@ -8,6 +8,6 @@ URL=$1
 
 curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
-sudo ./webimage.sh -b -p /intel/oneapi/intelpython -k
+sudo ./webimage.sh -b -p /intel/oneapi/intelpython
 installer_exit_code=$?
 exit $installer_exit_code
--- scripts/list_components_linux_aitools.sh
+++ scripts/list_components_linux_aitools.sh
@@ -9,7 +9,7 @@ FILENAME=$2
 
 curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
-sudo ./webimage.sh -b -p webimage_extracted -k
+sudo ./webimage.sh -b -p webimage_extracted
 rm -rf webimage.sh
 source webimage_extracted/bin/activate base
 installer_exit_code=$?
