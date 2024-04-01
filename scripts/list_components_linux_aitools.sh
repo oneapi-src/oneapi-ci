576c14095f80634c59da1bf356a83d64391d783e
Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>
2024-04-01 09:55:01 -0700

    Check memory
    Signed-off-by: Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>

--- scripts/list_components_linux_aitools.sh
+++ scripts/list_components_linux_aitools.sh
@@ -9,9 +9,8 @@ FILENAME=$2
 
 curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
-df -h
-sleep 10
-sudo ./webimage.sh -b -p webimage_extracted
+free -m
+./webimage.sh -b -p webimage_extracted
 rm -rf webimage.sh
 source webimage_extracted/bin/activate base
 installer_exit_code=$?
