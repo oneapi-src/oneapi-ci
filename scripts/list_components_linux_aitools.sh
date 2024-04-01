d8902e3f69b5c382eef8f01d68c9515054d0bb40
Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>
2024-04-01 10:50:30 -0700

    Modify path
    Signed-off-by: Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>

--- scripts/list_components_linux_aitools.sh
+++ scripts/list_components_linux_aitools.sh
@@ -11,7 +11,8 @@ curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
 ./webimage.sh -b -p webimage_extracted
 rm -rf webimage.sh
-source "$HOME"/webimage_extracted/bin/activate base
+pwd
+source "$HOME"/work/oneapi-ci/oneapi-ci/webimage_extracted/bin/activate base
 installer_exit_code=$?
 conda env list > "$FILENAME"
 cat "$FILENAME"
