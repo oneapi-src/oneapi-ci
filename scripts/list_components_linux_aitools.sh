cf3e5bb572765673e9532327f6a3393644721cec
Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>
2024-04-01 11:34:27 -0700

    Update script
    Signed-off-by: Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>

--- scripts/list_components_linux_aitools.sh
+++ scripts/list_components_linux_aitools.sh
@@ -9,11 +9,10 @@ FILENAME=$2
 
 curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
-./webimage.sh -b -p webimage_extracted
-rm -rf webimage.sh
 pwd
 df -h
-free -m
+./webimage.sh -b -p webimage_extracted
+rm -rf webimage.sh
 source "$HOME"/work/oneapi-ci/oneapi-ci/webimage_extracted/bin/activate base
 installer_exit_code=$?
 conda env list > "$FILENAME"
