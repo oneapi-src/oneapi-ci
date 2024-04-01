828a98eb70f431f1556c90992cade8a33484475d
Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>
2024-04-01 10:45:36 -0700

    Correct commit
    Signed-off-by: Jesus Hector Robles Gutierrez <jesus.hector.robles.gutierrez@intel.com>

--- scripts/list_components_linux_aitools.sh
+++ scripts/list_components_linux_aitools.sh
@@ -11,7 +11,7 @@ curl --output webimage.sh --url "$URL" --retry 5 --retry-delay 5
 chmod +x webimage.sh
 ./webimage.sh -b -p webimage_extracted
 rm -rf webimage.sh
-source $HOME/webimage_extracted/bin/activate base
+source "$HOME"/webimage_extracted/bin/activate base
 installer_exit_code=$?
 conda env list > "$FILENAME"
 cat "$FILENAME"
